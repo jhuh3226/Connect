aedb13cb5e55b70bb5d27d546651e1923c2c934e
Jung Huh <jhuh3226@gmail.com>
2021-02-15 22:56:32 -0500

    Journal

--- Week2Data/server.js
+++ Week2Data/server.js
@@ -32,30 +32,49 @@ app.post('/formdata', function (req, res) {
     dataToSave = req.body.data
     */
 
+    // random word generator to assist journal
+    // refer:https://codepen.io/chiragbhansali/pen/EWppvy?__cf_chl_captcha_tk__=2bba37ba415db3540512b721e5547653eed3ce3a-1613442593-0-AQ3W6nBRrdIEkbxtY-UDm8bYk617R1iqixsRG8pHlVMWQqmqXSfQhgucy0p1IAQG886iTAFqXT8YTNEmtwa2F0hSN-T5Y98r_yA7cGahFPmTg34X9u9OHh-jgjGddsfX3z0HtDX1s023SJi8Ga--7E4UWkNqt76ZaYkGyAQb2siRCkO1SzCzVgnCUMWKrEaj3jiWRUrCp8Tn7Wshwy-vqU2v2aAvOGScm4FJBkxoFaLLgM4oLN7_zBYuNByalyQC41R9C2HMqDs3C-8BK2RhpFKHhho3svHn48JSwCECTGCaFOr-TtZ56ubI1LaSA4GYYxUzHDoUGhoUeqMkRLzgGJ-tju5_Ip5G6jdZklm7qvhcuGzCPBhyNSBCV5GACMVCZTX4U-ROrWAj88RU21hIRsb7L8Ao-w57VQpht5uy5hLZkubZPHnXWrOAjAwP1_Xzbke6HV5-T7yiCyOWClnMCJ7nM3S10QRN3wPSTtf20POn8GBuqWNqE27otXN7FBzJrZoNrpuD2oV6TXquvZnW1et7lmsBksh6HPo4J5jX3yjy9a2MZkOon6BTFXwTFruGNTAbDjqrt-DZvaTKyYZNuY27TyxcdzFUnUsplErgSsui
+    var words;
+    var rand;
+    var content;
+    words = ["Why?", "For what?", "With who?", "How was it?", "Oh that happend"];
+
+    function randGen() {
+        return Math.floor(Math.random() * 5);
+    }
+
+    function sentence() {
+        rand = Math.floor(Math.random() * 5);
+        content = words[rand];
+    };
+
+    sentence();
+    //console.log(content);
+
     // JSON method
     var dataToSave = {
-        name: req.body.name,
-        phone: req.body.phone,
-        color: req.body.color,
-        image: req.body.image
+        time: req.body.time,
+        text: req.body.paragraph_text,
+        color: req.body.color
     };
 
     // storing input data and push
     submittedData.push(dataToSave);
-
     console.log(submittedData);
 
     // output the result
     var output = "<html><body>"
-    output += "<h1>Submitted Data</h1>";
+    output += "<h3 style='padding: 14px 16px'> Your Journal </h3>";
     for (var i = 0; i < submittedData.length; i++) {
-        //output += "<div id = mainText>" + submittedData[i].name + submittedData[i].phone + "</div>"
         //"div { background-color: #d5f4e6;}"
-        output += "<div style = 'color: " + submittedData[i].color + "'>" + submittedData[i].name + submittedData[i].phone + "</div>"
+        output += "<div style = 'color: " + submittedData[i].color + "; padding: 14px 16px'>" + submittedData[i].text + "<br>" + submittedData[i].time + "</div>"
     }
     output += "</body></html>";
 
-    res.send(output);
+    var goBackBT = "<html><body>" + "<p style='padding: 14px 16px'>Try to answer the question if you are stuck and back to continue your journal</p>" + "</body></html>";
+    var leadingQuestion = "<html><body>" + "<div style = 'padding: 14px 16px'>" + content + "</div>" + "</body></html>";
+    res.send(output + leadingQuestion + goBackBT);
+
     //send back
     //res.send("Got your data! You submitted: " + req.body.name + "phone" + req.body.phone + req.body.color);
 });
@@ -63,4 +82,4 @@ app.post('/formdata', function (req, res) {
 // 30 developement, 80 is default
 app.listen(8080, function () {
     console.log('Example app listening on port 8080!');
-})
+});
